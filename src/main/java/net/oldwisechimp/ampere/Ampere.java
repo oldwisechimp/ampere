c54d4c6ec1ebc9e296551ebff44f8313f1b3bd18
oldwisechimp <oldwisechimp@gmail.com>
2023-04-24 18:45:42 +0200

    Initial Commit

---
+++ src/main/java/net/oldwisechimp/ampere/Ampere.java
@@ -0,0 +1,47 @@
+package net.oldwisechimp.ampere;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Ampere.MOD_ID)
+public class Ampere
+{
+    public static final String MOD_ID = "ampere";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Ampere()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
